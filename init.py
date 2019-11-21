1060eefe58225b9c6e3eec7a15b51823dd298033
Robert Jeffrey <robertmkjeffrey@gmail.com>
2019-11-21 06:41:54 +0000

    Updated init file to create database.

--- init.py
+++ init.py
@@ -1,1 +1,7 @@
-CREATE TABLE tweets (id text, author text, content text, has_link text, has_video text, has_image text, is_reply text, is_retweet text, is_quote_rt text, notify text);
+import sqlite3
+
+# Set up database.
+connection = sqlite3.connect("database.db")
+with connection:
+    connection.execute("CREATE TABLE tweets (id text, author text, content text, has_link text, has_video text, has_image text, is_reply text, is_retweet text, is_quote_rt text, notify text);")
+connection.close()

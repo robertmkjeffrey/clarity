afb31b5a3e2d802c789a65cbf1def112e513b0d6
Robert Jeffrey <robertmkjeffrey@gmail.com>
2019-11-30 01:59:15 +0000

    Implemented basic streaming (finding new posts without parsing them at all).

--- deviantArt_streamer.go
+++ deviantArt_streamer.go
@@ -1,6 +1,8 @@
 package main 
 
 import (
+	"fmt"
+	"strconv"
 	"encoding/json"
 	"log"
 	"bytes"
@@ -10,29 +12,80 @@ import (
 	"time"
 )
 
+// Time to wait between polls of deviantArt
+const pollingRate = 5 * time.Minute
+
 const urlEncoded = "application/x-www-form-urlencoded"
+var dAFollows chan dAFollow // Circular channel of followed users
 
 // deviation implements the streamablePost interface, represeting a post drawn from deviantArt.
 type deviation struct {
 	id string
+	url string
 	json []byte
 }
 
+type dAFollow interface {
+	// getResults queries deviantArt for the most recent posts from the follow.
+	getResults(offset int) map[string]interface{}
+	getLastQueryTime() int64
+}
+
+type dATagFollow struct {
+	tag string
+	lastQueryTime int64
+}
+
+func (f dATagFollow) getLastQueryTime() int64 {
+	return f.lastQueryTime
+}
+
+func (f dATagFollow) getResults(offset int) map[string]interface{} {
+	params := url.Values{}
+	params.Add("tag", f.tag)
+	// params.Add("offset", string(offset))
+	
+	dAAccessToken.RLock()
+	params.Add("access_token", dAAccessToken.token)
+	dAAccessToken.RUnlock()
+
+	requestSting := params.Encode()
+
+	// Send request
+	resp, err := http.Get(fmt.Sprintf("https://www.deviantart.com/api/v1/oauth2/browse/tags?%s", requestSting))
+		
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Decode the results
+	var result map[string]interface{}
+	
+	json.NewDecoder(resp.Body).Decode(&result)
+	
+	return result
+}
+
+type dAUserFollow struct {
+	user string
+	lastQueryTime int64
+}
+
 // Global variable for access token storage.
-var deviantArtAccessToken struct {
+var dAAccessToken struct {
 	sync.RWMutex
 	token string
 }
 
-func getDeviantArtAccessToken() {
+func getDAAccessToken() {
 
-	deviantArtKeys := keys["deviantArt"].(map[interface{}]interface{})
+	dAKeys := keys["deviantArt"].(map[interface{}]interface{})
 	
 	// Build url encoding of request.
 	params := url.Values{}
 	params.Add("grant_type", "client_credentials")
-	params.Add("client_id", deviantArtKeys["client_id"].(string))
-	params.Add("client_secret", deviantArtKeys["client_secret"].(string))
+	params.Add("client_id", dAKeys["client_id"].(string))
+	params.Add("client_secret", dAKeys["client_secret"].(string))
 
 	requestSting := params.Encode()
 
@@ -57,28 +110,74 @@ func getDeviantArtAccessToken() {
 	}
 
 	// Set the token globally.
-	deviantArtAccessToken.Lock()
-	deviantArtAccessToken.token = token.(string)
-	deviantArtAccessToken.Unlock()
+	dAAccessToken.Lock()
+	dAAccessToken.token = token.(string)
+	dAAccessToken.Unlock()
 	
 }
 
-func (deviation) downloadStream(chan<- streamablePost) {
-	// TODO: make this work if spawned multiple times
-	// TODO: implement
+func dADownloadWorker(downloadQueue chan<- streamablePost) {
+	//TODO: implement
+	for {
+		// Get the next search and wait until the next polling opportunity
+		currentSearch := <-dAFollows
+		lastQueryTime := currentSearch.getLastQueryTime()
+		// Find time since last query
+		ellapsedTime := time.Since(time.Unix(lastQueryTime, 0))
+		// Wait for polling time from last query
+		time.After(pollingRate - ellapsedTime)
+		
+		nextPage := 0
+		dAResultParseLoop:
+		for {
+			query := currentSearch.getResults(nextPage)
+			// log.Println(query)
+			results := query["results"].([]interface{})
+			for _, result := range results {
+				result := result.(map[string]interface{})
+				// Parse the published time return value
+				publishedTime, err := strconv.ParseInt(result["published_time"].(string), 10,64)
+				if err != nil {
+					log.Fatalln(err)
+				}
+				// If the result is older than the last parse time, end the query.
+				if publishedTime <= lastQueryTime {
+					break dAResultParseLoop
+				}
+				// TODO: Make query to get deviation object etc.
+				downloadQueue <- deviation{}
+			}
+			nextPage++
+		}
+
+
+	}
+}
+
+func (deviation) createDownloadStream(downloadQueue chan<- streamablePost, workers int) {
 
 	// Request an access token.
-	getDeviantArtAccessToken()
+	getDAAccessToken()
 
 	// Every 59 minutes, get a new access token. Token expires every 60 minutes.
 	go func(){
 		for {
 		time.Sleep(59 * time.Minute)
-		getDeviantArtAccessToken()
+		getDAAccessToken()
 		}
 	}()
 
-	// Actually download data
+	// TODO: Read follow files from database.
+	dAFollows = make(chan dAFollow, 1)
+	dAFollows <- dATagFollow{tag:"vernid", lastQueryTime:1574935102}
+
+	// Spawn a worker for each in the range of workers.
+	for i := 0; i < workers; i++ {
+		go dADownloadWorker(downloadQueue)
+	}
+
+	return
+
 }
 
 func (d deviation) formatLink() string {

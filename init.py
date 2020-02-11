fc4ac7f21e034b3c4b2c1cf9f384057f70e80fa9
Robert Jeffrey <robertmkjeffrey@gmail.com>
2020-02-11 10:13:20 +0000

    Removed old project files.

--- init.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-# Set up database.
-connection = sqlite3.connect("database.db")
-with connection:
-    connection.execute("CREATE TABLE tweets (id text, author text, content text, has_link text, has_video text, has_image text, is_reply text, is_retweet text, is_quote_rt text, notify text);")
-connection.close()

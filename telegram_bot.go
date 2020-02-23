1da9f57f2e749d2b59d0d21ba78acb34dc084363
Robert Jeffrey <robertmkjeffrey@gmail.com>
2020-02-23 00:43:24 +0000

    Added placeholders for commands to be added and ran code cleanup.

--- telegram_bot.go
+++ telegram_bot.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"errors"
-	"strings"
-	"log"
 	"fmt"
-	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
+	"strings"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 func sendShutdownMessage(r interface{}) {
@@ -37,7 +38,7 @@ func sendPost(post streamablePost, score float64) {
 	telegramBot.Send(msg)
 }
 
-func siteSelectKeyboard () tgbotapi.ReplyKeyboardMarkup {
+func siteSelectKeyboard() tgbotapi.ReplyKeyboardMarkup {
 	keyboard := tgbotapi.ReplyKeyboardMarkup{}
 	keyboard.OneTimeKeyboard = true
 	keyboard.ResizeKeyboard = true
@@ -46,7 +47,7 @@ func siteSelectKeyboard () tgbotapi.ReplyKeyboardMarkup {
 		keyboard.Keyboard = append(keyboard.Keyboard, tgbotapi.NewKeyboardButtonRow(tgbotapi.NewKeyboardButton(site.prettySiteName())))
 	}
 	return keyboard
-} 
+}
 
 func parseSiteName(text string) (streamablePost, error) {
 	for _, site := range siteTypes {
@@ -55,13 +56,13 @@ func parseSiteName(text string) (streamablePost, error) {
 		}
 	}
 	return nil, errors.New("invalid site name")
-} 
+}
 
 func followHandler(update tgbotapi.Update) (waitForResponse bool, responseHandler interface{}) {
-	
-	site, returnErr := parseSiteName(update.Message.Text) 
+
+	site, returnErr := parseSiteName(update.Message.Text)
 	if returnErr != nil {
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Invalid site name, please restart. ")		
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Invalid site name, please restart. ")
 		_, err := telegramBot.Send(msg)
 		if err != nil {
 			log.Panicln(err)
@@ -88,75 +89,89 @@ func telegramCallbackHandler() {
 	var responseHandler func(tgbotapi.Update) (bool, interface{})
 
 	for update := range updates {
-	switch {
-	case update.CallbackQuery != nil:	
-		// If update is a callback, handle the keyboard button
-		// Answer callback.
-		telegramBot.AnswerCallbackQuery(tgbotapi.NewCallback(update.CallbackQuery.ID,""))
-		
-		fields := strings.Fields(update.CallbackQuery.Data);
-		button := fields[0]
-		site := fields[1]
-		id := fields[2]
-		// Switch over each button
-		switch button {
-		case "cb_hide":
-			// Hide message
-			telegramBot.DeleteMessage(tgbotapi.NewDeleteMessage(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Message.MessageID))
-			log.Printf("Hide post %s\n", id)
-		case "cb_delete":
-			// Delete post from the database.
-			deletePost(site, id)
-			// Hide message.
-			telegramBot.DeleteMessage(tgbotapi.NewDeleteMessage(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Message.MessageID))
-			log.Printf("Delete post %s\n", id)
-		case "cb_true":
-			// Update post notify status
-			updatePostNotify(site, id, true)
-			log.Printf("Set notification true on post %s\n", id)
-		case "cb_false":
-			// Update post notify status.
-			updatePostNotify(site, id, false)
-			log.Printf("Set notification false on post %s\n", id)
-		}
+		switch {
+		case update.CallbackQuery != nil:
+			// If update is a callback, handle the keyboard button
+			// Answer callback.
+			telegramBot.AnswerCallbackQuery(tgbotapi.NewCallback(update.CallbackQuery.ID, ""))
 
-	case update.Message != nil && waitingForResponse:
-		// If waiting for a response and got a message, run the response handler. 
-		waitingForResponse, newResponseHandler := responseHandler(update)
-		// If still waiting for a response, cast the handler.
-		if waitingForResponse {
-			responseHandler = newResponseHandler.(func(tgbotapi.Update) (bool, interface{}))
-		}
+			fields := strings.Fields(update.CallbackQuery.Data)
+			button := fields[0]
+			site := fields[1]
+			id := fields[2]
+			// Switch over each button
+			switch button {
+			case "cb_hide":
+				// Hide message
+				telegramBot.DeleteMessage(tgbotapi.NewDeleteMessage(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Message.MessageID))
+				log.Printf("Hide post %s\n", id)
+			case "cb_delete":
+				// Delete post from the database.
+				deletePost(site, id)
+				// Hide message.
+				telegramBot.DeleteMessage(tgbotapi.NewDeleteMessage(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Message.MessageID))
+				log.Printf("Delete post %s\n", id)
+			case "cb_true":
+				// Update post notify status
+				updatePostNotify(site, id, true)
+				log.Printf("Set notification true on post %s\n", id)
+			case "cb_false":
+				// Update post notify status.
+				updatePostNotify(site, id, false)
+				log.Printf("Set notification false on post %s\n", id)
+			}
 
-	case update.Message != nil && update.Message.IsCommand():
-		// handle commands
-		var msg tgbotapi.MessageConfig
-		switch update.Message.Command(){
-		case "start":
-			msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Welcome! Try /help to get a list of commands.")
-		case "follow":
-			// Open a dialogue to add a new query to the follow list.
-			msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Which site do you want to add a follow for?")
-			msg.ReplyMarkup = siteSelectKeyboard()
-			// Save that we're waiting for a response.
-			waitingForResponse = true
-			responseHandler = followHandler
+		case update.Message != nil && waitingForResponse:
+			// If waiting for a response and got a message, run the response handler.
+			waitingForResponse, newResponseHandler := responseHandler(update)
+			// If still waiting for a response, cast the handler.
+			if waitingForResponse {
+				responseHandler = newResponseHandler.(func(tgbotapi.Update) (bool, interface{}))
+			}
+
+		case update.Message != nil && update.Message.IsCommand():
+			// handle commands
+			var msg tgbotapi.MessageConfig
+			switch update.Message.Command() {
+			case "start":
+				msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Welcome! Try /help to get a list of commands.")
+			case "help":
+				msg = tgbotapi.NewMessage(update.Message.Chat.ID, "TODO: Fill this out")
+			case "follow":
+				// Open a dialogue to add a new query to the follow list.
+				msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Which site do you want to add a follow for?")
+				msg.ReplyMarkup = siteSelectKeyboard()
+				// Save that we're waiting for a response.
+				waitingForResponse = true
+				responseHandler = followHandler
+			case "retrain":
+				// TODO: trigger a certain model to be retrained based on the latest data.
+				msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Sorry, this feature hasn't been implemented yet! Message @DingoDingus for an update.")
+			case "stats":
+				// TODO: calculate and return statistics about the models
+				msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Sorry, this feature hasn't been implemented yet! Message @DingoDingus for an update.")
+			case "label":
+				// TODO: send a series of posts to be labelled based on active-learning maths.
+				msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Sorry, this feature hasn't been implemented yet! Message @DingoDingus for an update.")
+			default:
+				// If command isn't recognised, reply with error.
+				msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Sorry, I didn't understand what you said. Try /help for commands.")
+			}
+
+			if msg.Text != "" {
+				_, err := telegramBot.Send(msg)
+				if err != nil {
+					log.Panicln(err)
+				}
+			}
+
 		default:
 			// If command isn't recognised, reply with error.
-			msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Sorry, I didn't understand what you said. Try /help for commands.")		
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Sorry, I didn't understand what you said. Try /help for commands.")
+			_, err := telegramBot.Send(msg)
+			if err != nil {
+				log.Panicln(err)
+			}
 		}
-		_, err := telegramBot.Send(msg)
-		if err != nil {
-			log.Panicln(err)
-		}
-
-	default:
-		// If command isn't recognised, reply with error.
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Sorry, I didn't understand what you said. Try /help for commands.")		
-		_, err := telegramBot.Send(msg)
-		if err != nil {
-			log.Panicln(err)
-		}
-	}
 	}
 }
